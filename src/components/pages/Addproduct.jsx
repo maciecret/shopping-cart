edf9ffbefc8ffd80a5ad0c5d9000df12708f2c0d
maciecret <mercypulido4@gmail.com>
2023-02-18 22:10:11 +0800

    add json server

--- src/components/pages/Addproduct.jsx
+++ src/components/pages/Addproduct.jsx
@@ -1,7 +1,114 @@
 import React from 'react'
+import data from '../../data/data' 
+import { useState } from 'react';
+import axios from 'axios';
 
 export default function Addproduct() {
+
+  const API_URL = "http://localhost:5000/allProduct"
+
+ 
+
+  const [inputs, setInputs] = useState([]);
+  const [items, setItems] = useState([])
+  
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => (
+      {...values, [name]: value }))
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    
+    console.log(inputs)
+    await axios.post(API_URL, inputs);
+    alert('Data Inserted');
+  }
+
   return (
-    <div>Addproduct</div>
+    <div style = {{marginTop: "5%"}}>
+      <div class="row">  
+       <div className="col-sm-4 mx-auto shadow p-5">
+        <h2 className="text-center mb-4">Add A Product</h2>
+        
+        <form onSubmit={handleSubmit}>
+          
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Product Name"
+              name= "title"
+              value= {inputs.title || ""}
+              onChange={handleChange}
+            /><br/>
+          </div>
+
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Product Price"
+              name="price"
+              value={inputs.price || ""}
+              onChange={handleChange}
+            /><br/>
+          </div>
+
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Product Description"
+              name="desc"
+              value={inputs.desc || ""}
+              onChange={handleChange}
+            /><br/>
+          </div>
+
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Product Image URL"
+              name="img"
+              value={inputs.img || ""}
+              onChange={handleChange}
+            /><br/>
+          </div>
+
+          <div  style = {{display: "flex", justifyContent: "space-around"}}>
+          <button className="btn btn-primary btn-block"  style = {{backgroundColor: "#86a4ba", border: "none"}}>Add Product</button>
+          </div>
+
+        </form>
+      </div>
+    </div>
+  </div>  
+
+
+
+
+    // <form onSubmit={handleSubmit}>
+    //   <label>Product Name
+    //   <input 
+    //     type="text" 
+    //     name="title" 
+    //     value={inputs.title|| ""} 
+    //     onChange={handleChange}
+    //   />
+    //   </label>
+    //   <label>Price
+    //     <input 
+    //       type="number" 
+    //       name="price" 
+    //       value={inputs.price || ""} 
+    //       onChange={handleChange}
+    //     />
+    //     </label>
+    //     <input type="submit" />
+    // </form>
   )
 }
--- src/components/pages/Allproducts.jsx
+++ src/components/pages/Allproducts.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Itemcard from '../Itemcard'
-import data from '../../data/data'
+
 import { Link } from 'react-router-dom'
 
 export default function Allproducts() {
+  const API_URL = "http://localhost:5000/allProduct"
+  const[items,setItems] = useState([])
+
+  // -- FETCHING DATA FROM API -- //
+
+  useEffect(() =>{
+
+    const fetchItems = async() => {
+      try {
+        const response = await fetch(API_URL);
+        const listItems = await response.json();
+        console.log(listItems)
+        setItems(listItems)
+        
+      } catch (error) {
+        console.log(error.stack)
+        
+      }
+
+    }
+    (async() => await fetchItems())();
+
+   
+  },[])
+
+  
+  
   return (
     <div>
       <h1 className = "text-center mt-3"  style = {{ color: "#576179"}}>All Products</h1>
@@ -13,7 +40,7 @@ export default function Allproducts() {
         <section className='py-4 container'>
           <div className='row justify-content-center'>
 
-                {data.allProduct.map((item, index) => {
+                {items.map((item, index) => {
                     return(
                       <Itemcard 
                       img = {item.img} 
